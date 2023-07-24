c674f950aeee7f4816d10488d04ff8e585e75e48
martaze blackmon <martazeb47@gmail.com>
2023-07-24 00:25:14 -0400

    Updated Selction Statements

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -6,27 +6,27 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            //Number Guessing Game
-            //var r = new Random();
-            //var favNumber = r.Next(0, 1001);
+            Number Guessing Game
+            var r = new Random();
+            var favNumber = r.Next(0, 1001);
 
 
-            //Console.WriteLine("Pick a number from 1 - 1000");
-            //var userInput = int.Parse(Console.ReadLine());
+            Console.WriteLine("Pick a number from 1 - 1000");
+            var userInput = int.Parse(Console.ReadLine());
 
-            //if (userInput < favNumber)
-            //{
-            // Console.WriteLine($"{userInput} is too low.");
-            //}
-            //else if (userInput > favNumber) 
-            //{
-            //Console.WriteLine($"{userInput} is too high.");
-            //}
-            //else
-            //{
-            //Console.WriteLine("You guessed it.");
-            //}
-            //Switch Case
+            if (userInput < favNumber)
+            {
+             Console.WriteLine($"{userInput} is too low.");
+            }
+            else if (userInput > favNumber) 
+            {
+            Console.WriteLine($"{userInput} is too high.");
+            }
+            else
+            {
+            Console.WriteLine("You guessed it.");
+            }
+            Switch Case
 
             Console.WriteLine("What is your favorite subject?");
 
